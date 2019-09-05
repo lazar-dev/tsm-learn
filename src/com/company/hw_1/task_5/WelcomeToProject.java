7432971b89a9556fcc929572b13d7946663ea3ce
Lazar <you@example.com>
2019-09-05 22:35:31 +0300

    Home Word 1

---
+++ src/com/company/hw_1/task_5/WelcomeToProject.java
@@ -0,0 +1,7 @@
+package com.company.hw_1.task_5;
+
+public class WelcomeToProject {
+    public static void main(String[] args) {
+        System.out.println("Hello " + args[0] + ", Welcome to my first project!");
+    }
+}
