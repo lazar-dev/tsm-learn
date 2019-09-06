cc57f3fed6cad15ea5dbdf44004713e5d9940fc9
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-06 09:57:48 +0300

    Update WelcomeToProject.java

--- src/com/company/hw_1/task_5/WelcomeToProject.java
+++ src/com/company/hw_1/task_5/WelcomeToProject.java
@@ -1,5 +1,5 @@
 package com.company.hw_1.task_5;
-
+// WelcomeToProject -> WelcomeProject лучше так
 public class WelcomeToProject {
     public static void main(String[] args) {
         System.out.println("Hello " + args[0] + ", Welcome to my first project!");
