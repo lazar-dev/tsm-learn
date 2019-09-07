f46d7e8b4e429a6bcffb7f05fe69807f8d6176f4
Lazar <you@example.com>
2019-09-07 18:41:18 +0300

    rename class

--- src/com/company/hw_1/task_5/WelcomeToProject.java
+++
@@ -1,7 +0,0 @@
-package com.company.hw_1.task_5;
-
-public class WelcomeToProject {
-    public static void main(String[] args) {
-        System.out.println("Hello " + args[0] + ", Welcome to my first project!");
-    }
-}
