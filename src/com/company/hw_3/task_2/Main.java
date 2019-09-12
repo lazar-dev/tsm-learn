ba3eb5d1b2950078c449e491d42c852dc35391c4
Lazar <you@example.com>
2019-09-12 09:25:19 +0300

    HW_3 TASK 2

---
+++ src/com/company/hw_3/task_2/Main.java
@@ -0,0 +1,14 @@
+package com.company.hw_3.task_2;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = -1;
+        int z = 0;
+        x = z - x;
+        z = -x + z;
+        x = z+x;
+        System.out.println(x);
+        System.out.println(z);
+
+    }
+}
