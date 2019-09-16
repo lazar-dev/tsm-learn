8d00904f516602c3cb6a72c95383f926d2a24b08
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-16 21:37:56 +0300

    Update Computer.java

--- src/com/company/hw_3/task_3/Computer.java
+++ src/com/company/hw_3/task_3/Computer.java
@@ -2,6 +2,7 @@ package com.company.hw_3.task_3;
 
 public interface Computer {
     String enable();
+    // disa -> disable, не стоит делать таких сокращений
     String disa();
     String reset();
 
