7ed3ddf57f3059d3c4b58fdacaf474323f8d54aa
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-16 21:30:45 +0300

    Update PersonMain.java

--- src/com/company/hw_3/task_1/PersonMain.java
+++ src/com/company/hw_3/task_1/PersonMain.java
@@ -1,8 +1,9 @@
 package com.company.hw_3.task_1;
 
+// PersonMain -> Task_1 - для удобства, это будет значить, что в этом классе метод main для таски
 public class PersonMain {
     public static void main(String[] args) {
-        Person noName = new Person(); //Дефолт
+        Person noName = new Person(); //Дефолт - не оставляй такие комментарии. Итак видно, что конструктор дефолт
         System.out.println(noName.toString());
 
         Person Ivan = new Person("Ivan",28);
