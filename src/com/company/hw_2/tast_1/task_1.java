037d5bbdc1aa42cf677bf29b5cb061d2f0b9522d
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-16 21:22:50 +0300

    Update task_1.java

--- src/com/company/hw_2/tast_1/task_1.java
+++ src/com/company/hw_2/tast_1/task_1.java
@@ -1,5 +1,7 @@
 package com.company.hw_2.tast_1;
 
+// Название класса с большой буквы
+// название пакета с ошибкой написано
 public class task_1{
     public static void main(String[] args) {
         int myInt = 1;
