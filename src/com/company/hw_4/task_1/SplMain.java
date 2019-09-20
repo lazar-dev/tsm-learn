e9c044f39f6ab5d53e5d26d38d356d6113737fba
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-20 09:34:50 +0300

    Update SplMain.java

--- src/com/company/hw_4/task_1/SplMain.java
+++ src/com/company/hw_4/task_1/SplMain.java
@@ -1,7 +1,10 @@
 package com.company.hw_4.task_1;
 
+// если я верно понял, то Spl = split, не делай таких сокращений. 
+// Класс с методом main можно называть по номеру таски, так удобнее
 public class SplMain {
     public static void main(String[] args) {
+        // str -> wordToSplit
         String str = "engineering";
         System.out.println(str.substring(0,6)); //engine
         System.out.println(str.substring(7)); //ring
