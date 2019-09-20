0fc7659f52b8b45b57e2e46cbd1d36b7ecfc121b
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-20 09:37:26 +0300

    Update Main.java

--- src/com/company/hw_4/task_2/Main.java
+++ src/com/company/hw_4/task_2/Main.java
@@ -1,7 +1,9 @@
 package com.company.hw_4.task_2;
 
+// Main -> Task_2
 public class Main {
     public static void main(String[] args) {
+        // str -> wordToReverse
         String str = "alucard";
         str = new StringBuilder(str).reverse().toString();
         System.out.println(str);
