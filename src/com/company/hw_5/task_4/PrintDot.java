b36bf93909f57f1f56bcb584b08f6f6bf11e1e1c
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-21 12:12:16 +0300

    Update PrintDot.java

--- src/com/company/hw_5/task_4/PrintDot.java
+++ src/com/company/hw_5/task_4/PrintDot.java
@@ -2,6 +2,7 @@ package com.company.hw_5.task_4;
 
 public class PrintDot {
     public static void main(String[] args) {
+        // 6 - вынеси в отдельную переменную
         for (int i = 6; i > 0 ; i--) {
             String dot = "";
             for (int j = 0; j < i; j++) {
