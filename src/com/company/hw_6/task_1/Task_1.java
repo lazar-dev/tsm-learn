47e0df6a7eea18b8a1554a405741026dfc4baf65
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-25 21:25:18 +0300

    Update Task_1.java

--- src/com/company/hw_6/task_1/Task_1.java
+++ src/com/company/hw_6/task_1/Task_1.java
@@ -3,8 +3,10 @@ package com.company.hw_6.task_1;
 import java.math.BigDecimal;
 import java.math.MathContext;
 
+// форматируй код
 public class Task_1 {
     public static void main(String[] args) {
+        // myDivide -> result
         BigDecimal myDivide = divide(8, 0);
         System.out.println(myDivide.toString());
     }
@@ -16,6 +18,8 @@ public class Task_1 {
             return bigDecimalX.divide(bigDecimalY, MathContext.DECIMAL128);
         }catch (ArithmeticException e){
             System.out.println("Делить на ноль нельзя!");
+            // так делать не стоит. Лучше выбрасывать какой-нибудь RuntimeException("сообщение"),
+            // тогда компилятор не будет ругаться что метод ничего не возвращает
             return  new BigDecimal(0);
         }
     }
