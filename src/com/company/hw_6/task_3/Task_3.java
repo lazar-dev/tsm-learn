2c6206e5d83c48bd755e14917851f03da669ff74
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-25 21:28:29 +0300

    Update Task_3.java

--- src/com/company/hw_6/task_3/Task_3.java
+++ src/com/company/hw_6/task_3/Task_3.java
@@ -16,6 +16,7 @@ public class Task_3 {
         }
     }
 
+    // method createArray должен просто создавать массив. Это исключение надо бросать в методе, где вводится число
     private static int[] createArray(int sizeArray) throws OversizeArrayExeption {
         if (sizeArray <= 25) {
             int[] intArrays = new int[sizeArray];
@@ -27,11 +28,12 @@ public class Task_3 {
         throw new OversizeArrayExeption("Превышен размер массива");
     }
 
+    // scanerIntThread -> scanner -> зачем Thread в названии? getIntFromUser, например
     private static int scanerIntThread() {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите размер массива: ");
         return in.nextInt();
     }
 
-
+// линяя пустая строка
 }
