369d9a53983fb6a34b944c8b55bb0e904ae5df79
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-14 21:32:27 +0300

    Update Task_1.java

--- src/com/company/hw_11/task_1/Task_1.java
+++ src/com/company/hw_11/task_1/Task_1.java
@@ -1,5 +1,6 @@
 package com.company.hw_11.task_1;
 
+// +
 public class Task_1 {
     public static void main(String[] args) throws InterruptedException {
         Thread myThread = new Thread(new IterationRunnuble());
