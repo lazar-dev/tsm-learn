35cb871c21fdcca105ac97c2cdfcec843f2d1dab
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-14 21:32:51 +0300

    Update IterationSleepRunnuble.java

--- src/com/company/hw_11/task_2/IterationSleepRunnuble.java
+++ src/com/company/hw_11/task_2/IterationSleepRunnuble.java
@@ -1,9 +1,11 @@
 package com.company.hw_11.task_2;
 
+// RunnAble
 public class IterationSleepRunnuble implements Runnable {
 
     @Override
     public void run() {
+                // 20 -лучше через конструктор передавать
         for (int i = 0; i < 20; i++) {
             try {
                 System.out.println(i);
