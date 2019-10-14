4aea53dcabd2623279a91ea89b54bf9ea8f8a370
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-14 21:33:11 +0300

    Update Task_2.java

--- src/com/company/hw_11/task_2/Task_2.java
+++ src/com/company/hw_11/task_2/Task_2.java
@@ -1,5 +1,6 @@
 package com.company.hw_11.task_2;
 
+// +
 public class Task_2 {
     public static void main(String[] args) throws InterruptedException {
         Thread mySleepThread = new Thread(new IterationSleepRunnuble());
