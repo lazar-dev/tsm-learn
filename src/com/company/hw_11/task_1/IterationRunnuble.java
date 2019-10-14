10469ff598d479d75b659543b14bd8b5436aa08c
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-14 21:32:10 +0300

    Update IterationRunnuble.java

--- src/com/company/hw_11/task_1/IterationRunnuble.java
+++ src/com/company/hw_11/task_1/IterationRunnuble.java
@@ -1,9 +1,11 @@
 package com.company.hw_11.task_1;
 
+// RunnAble
 public class IterationRunnuble implements Runnable {
 
     @Override
     public void run() {
+         // 10 -лучше через конструктор передавать
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
         }
